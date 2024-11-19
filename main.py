b48b859ac9ee2ba8976e68b61014198451a43cca
Pol <example@example.com>
2024-11-19 01:30:33 +0100

    Update requirements, remove unused imports, added some comments + other fixes

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from pathlib import Path
-from shutil import rmtree
 from phi.model.ollama import Ollama
-import requests
 from phi.agent import Agent
 from phi.vectordb.pgvector import PgVector
 from phi.knowledge.json import JSONKnowledgeBase
-from phi.knowledge.text import TextKnowledgeBase
 import parser.utils.utils as utils
 from phi.embedder.ollama import OllamaEmbedder
+import os
+import shutil
 
 dataExcel = "data/"
 
@@ -26,10 +24,13 @@ knowledge_base= JSONKnowledgeBase(
 
         embedder=OllamaEmbedder(host=ollamaBase, model="nomic-embed-text", dimensions=768),
     ),
-    # 2 references are added to the prompt
 )
 
+# DESCOMENTAR SI ES VOLEN CARREGAR ELS ARXIUS DE tmp/ A LA BASE DE CONEIXEMENT
+
 #knowledge_base.load(upsert=True)
+
+shutil.rmtree(tmpDir)
 
 agent = Agent(
     model=Ollama(host=ollamaBase, id="llama3.1"),
@@ -40,7 +41,5 @@ agent = Agent(
     search_knowledge=True,
     add_references_to_prompt=True,
 )
-
-#agent.knowledge.load(recreate=False)
 
 agent.print_response("Retrieve the auto avaluation grades & co-avaluation (if existent) tell me the teacher comments if they did put a comment. Keep in mind that the grades were put by students, there might be a teacher comment, but it's not for certain in all comments.", stream=True)
